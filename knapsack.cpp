30a93d725d38e0ad6cbec94342c7afe4e82e6d2d
ankitbhadoria21 <ankit.bhadoria21@gmail.com>
2015-11-29 15:31:17 -0500

    right file

--- knapsack.cpp
+++ knapsack.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int max_e=0;
 void solve(int arr[],int sum,int i,int k,int n)
 {
-if(i>=n)
+if(i>=n || sum>k)
 return  ;
 if(max_e<sum && sum<=k)
 max_e=sum;
-if(sum+arr[i]>k)
+solve(arr,sum+arr[i],i,k,n);
 solve(arr,sum,i+1,k,n);
-else solve(arr,sum+arr[i],i,k,n);
 
 }
 
